0d9bc1a7da2d0bdefddfbf251a389edb5bc60762
cooldude1103 <ujjwal.sbps@gmail.com>
2021-02-14 14:42:39 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/womensafety/Tips.java
@@ -0,0 +1,15 @@
+package com.example.womensafety;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Tips extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tips);
+        getSupportActionBar().setTitle("Tips");
+    }
+}
